b0c9db7fb8ffc28420e9f95c293872c4b76dd5d8
jfenuk <jfenuk@gmail.com>
2015-07-30 14:28:42 +0300

    fix.

--- src/main/java/com/fenuk/example/main/App.java
+++ src/main/java/com/fenuk/example/main/App.java
@@ -6,27 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.fenuk.example.config.AppConfig;
-import com.fenuk.example.dao.EmployeeDao;
 import com.fenuk.example.model.Employee;
+import com.fenuk.example.repository.EmployeeJdbcRepository;
 
 public class App {
 
 	@Autowired
-	EmployeeDao eDao;
+	EmployeeJdbcRepository eDao;
 
 	public void run() {
 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
 				AppConfig.class);
-		EmployeeDao dao = context.getBean(EmployeeDao.class);
+		EmployeeJdbcRepository dao = context.getBean(EmployeeJdbcRepository.class);
 
 		for (int i = 0; i < 10; i++) {
 
 			String name = RandomStringUtils.randomAlphabetic(10);
 			long salary = RandomUtils.nextLong(0, 10000);
 
-			dao.saveEmployee(new Employee(name, salary));
-			Employee e = dao.getEmployeeByName(name);
+			dao.save(new Employee(name, salary));
+			Employee e = dao.getByName(name);
 
 			System.out.println(i+": "+e);
 		}
